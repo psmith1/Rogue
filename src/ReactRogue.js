5a6bc97f1a3d21e6bf9c590dbc38e7e108386dc7
Peter Smith <petergbsmith@gmail.com>
2021-01-04 14:24:41 -0600

    ready player one

--- src/ReactRogue.js
+++ src/ReactRogue.js
@@ -1,14 +1,33 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
+import InputManager from './InputManager.js';
 
 const ReactRogue = ({width, height, tilesize}) => {
     const canvasRef = React.useRef();
+    const [player, setPlayer] = useState({x:64, y:128 });
+    var inputManager = new InputManager();
+    const handleInput = (action, data) => {
+        console.log(`handle input: ${action}:${JSON.stringify(data)}`)
+        let newPlayer = { ...player };
+        newPlayer.x += data.x * tilesize;
+        newPlayer.y += data.y * tilesize;
+        setPlayer(newPlayer);
+    }
+    useEffect(() => {
+        console.log('Bind input');
+        inputManager.bindKeys();
+        inputManager.subscribe(handleInput);
+        return () => {
+            inputManager.unbindKeys();
+            inputManager.unsubscribe(handleInput);
+        }
+    })
     useEffect(() => {
         console.log("Draw to canvas");
         const ctx = canvasRef.current.getContext('2d');
         // Defines the context of the 2d canvas; determines syntax (clearRect, fillStyle, etc.) below
         ctx.clearRect(0, 0, width * tilesize, height * tilesize);
         ctx.fillStyle = '#000';
-        ctx.fillRect(12, 22, 16, 16);
+        ctx.fillRect(player.x, player.y, 16, 16);
     });
     return (
         <canvas
