544c2c807b5be69b64f0216f99fa9d874b3fe7f7
Peter Smith <petergbsmith@gmail.com>
2021-01-04 13:32:41 -0600

    added gameboard component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactRogue from './ReactRogue';
+
+const App = () => (
+    <div className="App">
+      <ReactRogue />
+    </div>
+);
+
+export default App;
---
+++ src/ReactRogue.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReactRogue = () => (
+    <canvas
+        width="256"
+        height="256"
+        style={{ border: '1px solid black' }}
+    ></canvas>
+        );
+
+export default ReactRogue;
